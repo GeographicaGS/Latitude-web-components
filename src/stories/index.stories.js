81bb05a262bf68c648abc0b2e41eeab0c6178c09
Alberto Arana <aranaboza@gmail.com>
2019-03-25 10:51:43 +0100

    rollback to web-components

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,48 +1,61 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/vue';
-import { action } from '@storybook/addon-actions';
+// import { action } from '@storybook/addon-actions';
 // import { linkTo } from '@storybook/addon-links';
 
-import Button from '@/components/button';
-import ButtonYellow from '@/components/button-yellow';
+import { withKnobs } from '@storybook/addon-knobs';
+
+import './main';
+
 
 storiesOf('Button', module)
+  .addDecorator(withKnobs)
   .add('with text', () => ({
-    components: {
-      'ltd-button': Button,
-    },
     data() {
       return {
+        el: undefined,
       };
     },
     template: `
-    <ltd-button theme='light' @click="onClick">
+    <ltd-button theme='light'>
       <span>A Button with rounded edges</span>
     </ltd-button>
   `,
+    mounted() {
+      [this.el] = document.getElementsByTagName('ltd-button');
+
+      this.el.addEventListener('click', this.onClick);
+    },
+    beforeDestroyed() {
+      this.el.removeEventListener('click', this.onClick);
+    },
     methods: {
       onClick(event) {
-        action('clicked');
         console.info('event', event);
       },
     },
   }))
   .add('yellow', () => ({
-    components: {
-      'ltd-button-yellow': ButtonYellow,
-    },
     data() {
       return {
+        el: undefined,
       };
     },
     template: `
-    <ltd-button-yellow @click="onClick">
+    <ltd-button-yellow>
       <span>A Button with rounded edges</span>
     </ltd-button-yellow>
   `,
+    mounted() {
+      [this.el] = document.getElementsByTagName('ltd-button-yellow');
+
+      this.el.addEventListener('click', this.onClick);
+    },
+    beforeDestroyed() {
+      this.el.removeEventListener('click', this.onClick);
+    },
     methods: {
       onClick(event) {
-        action('clicked');
         console.info('event', event);
       },
     },
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,46 @@
+function enableShadowCss(config) {
+  const configs = [
+    config.module.rule('vue').use('vue-loader'),
+    config.module.rule('css').oneOf('vue-modules').use('vue-style-loader'),
+    config.module.rule('css').oneOf('vue').use('vue-style-loader'),
+    config.module.rule('css').oneOf('normal-modules').use('vue-style-loader'),
+    config.module.rule('css').oneOf('normal').use('vue-style-loader'),
+    config.module.rule('postcss').oneOf('vue-modules').use('vue-style-loader'),
+    config.module.rule('postcss').oneOf('vue').use('vue-style-loader'),
+    config.module.rule('postcss').oneOf('normal-modules').use('vue-style-loader'),
+    config.module.rule('postcss').oneOf('normal').use('vue-style-loader'),
+    config.module.rule('scss').oneOf('vue-modules').use('vue-style-loader'),
+    config.module.rule('scss').oneOf('vue').use('vue-style-loader'),
+    config.module.rule('scss').oneOf('normal-modules').use('vue-style-loader'),
+    config.module.rule('scss').oneOf('normal').use('vue-style-loader'),
+    config.module.rule('sass').oneOf('vue-modules').use('vue-style-loader'),
+    config.module.rule('sass').oneOf('vue').use('vue-style-loader'),
+    config.module.rule('sass').oneOf('normal-modules').use('vue-style-loader'),
+    config.module.rule('sass').oneOf('normal').use('vue-style-loader'),
+    config.module.rule('less').oneOf('vue-modules').use('vue-style-loader'),
+    config.module.rule('less').oneOf('vue').use('vue-style-loader'),
+    config.module.rule('less').oneOf('normal-modules').use('vue-style-loader'),
+    config.module.rule('less').oneOf('normal').use('vue-style-loader'),
+    config.module.rule('stylus').oneOf('vue-modules').use('vue-style-loader'),
+    config.module.rule('stylus').oneOf('vue').use('vue-style-loader'),
+    config.module.rule('stylus').oneOf('normal-modules').use('vue-style-loader'),
+    config.module.rule('stylus').oneOf('normal').use('vue-style-loader'),
+  ];
+
+  configs.forEach(c => c.tap((options) => {
+    const data = options;
+    if (data) {
+      data.shadowMode = !!process.env.SHADOW_CSS;
+    }
+
+    return data;
+  }));
+}
+
 module.exports = {
   lintOnSave: false,
+  // https://cli.vuejs.org/guide/webpack.html#chaining-advanced
+  chainWebpack: (config) => {
+    enableShadowCss(config);
+  },
 };
