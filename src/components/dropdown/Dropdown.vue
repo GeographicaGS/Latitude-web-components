1c1db17d7b0880d39113a416ce3fb72ee3cf91c1
builtbyedgar <hello@builtbyedgar.com>
2019-03-26 14:58:09 +0100

    working on dropdown

---
+++ src/components/dropdown/Dropdown.vue
@@ -0,0 +1,9 @@
+/**
+ *  Dropdown.vue
+**/
+/* template import */
+<template src="./dropdown.html"></template>
+/* script import */
+<script src="./dropdown.js"></script>
+/* style import */
+<style scoped lang="scss" src="./dropdown.scss"></style>
--- src/stories/main.js
+++ src/stories/main.js
@@ -5,11 +5,14 @@ import wrap from '@vue/web-component-wrapper';
 import Icon from '@/components/icon/index'
 import Calendar from '@/components/calendar/index'
 import Table from '@/components/table/index'
+import Dropdown from '@/components/dropdown/index'
 
 const WrappedIcon = wrap(Vue, Icon)
 const WrappedCalendar = wrap(Vue, Calendar)
 const WrappedTable = wrap(Vue, Table)
+const WrappedDropdown = wrap(Vue, Dropdown)
 
 window.customElements.define('ltd-icon', WrappedIcon)
 window.customElements.define('ltd-calendar', WrappedCalendar)
 window.customElements.define('ltd-table', WrappedTable)
+window.customElements.define('ltd-dropdown', WrappedDropdown)
