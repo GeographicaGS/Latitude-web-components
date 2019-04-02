29d4ab3014fcc8791f2bea7762420554c8a4def3
builtbyedgar <hello@builtbyedgar.com>
2019-04-02 20:41:03 +0200

    dropdown example

--- src/stories/dropdown.stories.js
+++ src/stories/dropdown.stories.js
@@ -5,14 +5,15 @@ storiesOf('Dropdown', module)
   .add('Simple dropdown', () => ({
     data () {
       return {
-        el: undefined
+        el: undefined,
+        date: 'No date selected'
       }
     },
     directives: { },
     template: // html
       `
       <div>
-        <ltd-dropdown @select="onSelect">
+        <ltd-dropdown>
           <button slot="trigger">dropdown</button>
           <div slot="content">
             <div style="width: 200px; height: auto; border: 1px solid grey; border-radius: 3px; padding: 10px;">
@@ -24,11 +25,16 @@ storiesOf('Dropdown', module)
         <div>Some content here</div>
         <br />
         <ltd-dropdown>
-          <div slot="trigger">dropdown</div>
-          <div slot="content">
-            <div style="width: 200px; height: auto; border: 1px solid grey; border-radius: 3px; padding: 10px;">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas vitae dolor id sodales. Integer consectetur, magna nec euismod lobortis, mi felis tincidunt dolor, ac congue tellus mi tristique ante. Vestibulum sit amet fermentum dolor, vitae efficitur elit. Fusce vel quam urna. Curabitur accumsan orci ac fermentum volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus felis erat, iaculis vel euismod eget, iaculis sit amet felis. Maecenas suscipit vel nunc sit amet faucibus. Maecenas urna lacus, aliquam nec erat eu, vestibulum viverra sem. Quisque fringilla lorem eu ligula finibus, id cursus diam bibendum.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas vitae dolor id sodales. Integer consectetur, magna nec euismod lobortis, mi felis tincidunt dolor, ac congue tellus mi tristique ante. Vestibulum sit amet fermentum dolor, vitae efficitur elit. Fusce vel quam urna. Curabitur accumsan orci ac fermentum volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus felis erat, iaculis vel euismod eget, iaculis sit amet felis. Maecenas suscipit vel nunc sit amet faucibus.</p>
+          <div slot="trigger">{{ this.date }}</div>
+          <div slot="content" style="width: 300px;">
+            <div style="width: 282px; height: auto; box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.2); border-radius: 4px;">
+              <ltd-calendar
+                selected-date="2019-04-02"
+                month-format="MMMM"
+                day-format="ddd"
+                locale="en"
+                future-selection>
+              </ltd-calendar>
             </div>
           </div>
         </ltd-dropdown>
@@ -36,14 +42,19 @@ storiesOf('Dropdown', module)
     `,
     mounted () {
       [this.el] = document.getElementsByTagName('ltd-dropdown')
-      setTimeout(() => {
-        this.el.open = true
-      }, 2000)
+      const calendar = document.getElementsByTagName('ltd-calendar')[0]
+      calendar.dateChanged = this.onSetCalendarDate
+      console.log(calendar)
+
+      // setTimeout(() => {
+      //   this.el.open = true
+      // }, 2000)
     },
     beforeDestroyed () { },
     methods: {
-      onSelect () {
-        console.log('change')
+      onSetCalendarDate (value) {
+        console.log('The date selected is: ')
+        console.log(value.date)
       }
     }
   }))
