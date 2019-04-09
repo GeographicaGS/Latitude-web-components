52afaaa67312690a0f1bedbc0520e934735bbc73
builtbyedgar <hello@builtbyedgar.com>
2019-04-09 12:09:21 +0200

    removed dropdown component

--- src/components/dropdown/Dropdown.vue
+++
@@ -1,9 +0,0 @@
-/**
- *  Dropdown.vue
-**/
-/* template import */
-<template src="./dropdown.html"></template>
-/* script import */
-<script src="./dropdown.js"></script>
-/* style import */
-<style scoped lang="scss" src="./dropdown.scss"></style>
--- src/stories/main.js
+++ src/stories/main.js
@@ -5,7 +5,6 @@ import wrap from '@vue/web-component-wrapper';
 import Icon from '@/components/icon/index'
 import Calendar from '@/components/calendar/index'
 import Table from '@/components/table/index'
-import Dropdown from '@/components/dropdown/index'
 import Switch from '@/components/switch/index'
 import Checkbox from '@/components/checkbox/index'
 import Tooltip from '@/components/tooltip/index'
@@ -17,7 +16,6 @@ import Datepicker from '@/components/datepicker/index'
 const WrappedIcon = wrap(Vue, Icon)
 const WrappedCalendar = wrap(Vue, Calendar)
 const WrappedTable = wrap(Vue, Table)
-const WrappedDropdown = wrap(Vue, Dropdown)
 const WrappedSwitch = wrap(Vue, Switch)
 const WrappedCheckbox = wrap(Vue, Checkbox)
 const WrappedRadioGroup = wrap(Vue, RadioGroup)
@@ -29,7 +27,6 @@ const WrappedDatepicker = wrap(Vue, Datepicker)
 window.customElements.define('ltd-icon', WrappedIcon)
 window.customElements.define('ltd-calendar', WrappedCalendar)
 window.customElements.define('ltd-table', WrappedTable)
-window.customElements.define('ltd-dropdown', WrappedDropdown)
 window.customElements.define('ltd-switch', WrappedSwitch)
 window.customElements.define('ltd-checkbox', WrappedCheckbox)
 window.customElements.define('ltd-radio', WrappedRadio)
